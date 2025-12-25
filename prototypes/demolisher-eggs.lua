00a4e026a1428b1e978df45678f573c0dc14aea4
d.watanabe <daisuke.watanabe@citron-reiwa.com>
2025-12-25 21:09:14 +0900

    0.3.8 added clear tech

--- prototypes/demolisher-eggs.lua
+++ prototypes/demolisher-eggs.lua
@@ -1282,4 +1282,38 @@ data:extend({
 		},
 		order = "z-a"
 	}
+	,
+	{
+		type = "technology",
+		name = "technology-breeding-demolisher-clear",
+		icon_size = 256,
+		icon = "__BreedingDemolisher__/graphics/technology/technology-breeding-demolisher-clear.png",
+		prerequisites = {"demolisher-egg-unfreeze"},	-- Previous research as a prerequisite
+		unit = {
+			count_formula = "1000000*(L^1.5)",	-- Formula for increasing cost per level
+			ingredients = {
+			{"automation-science-pack", 1},
+			{"logistic-science-pack", 1},
+			{"military-science-pack", 1},
+			{"chemical-science-pack", 1},
+			{"production-science-pack", 1},
+			{"utility-science-pack", 1},
+			{"space-science-pack", 1},
+			{"metallurgic-science-pack", 1},
+			{"electromagnetic-science-pack", 1},
+			{"agricultural-science-pack", 1},
+			{"cryogenic-science-pack", 1},
+			{"promethium-science-pack", 1}
+			},
+			time = 60
+		},
+		max_level = "infinite",	-- Infinite research
+		effects = {
+			{
+			type = "nothing",
+			effect_description = {"technology-effect-quality.nothing"}
+			}
+		},
+		order = "z-a"
+	}
 })
